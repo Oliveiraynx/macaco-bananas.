8d95a416bb6e430fed5dacfb1d2d00d22e61104a
Oliveiraynx <90041161+Oliveiraynx@users.noreply.github.com>
2026-03-25 12:28:20 -0300

    meu_codigo.py
    Meu primeiro codigo

--- meu_codigo.py
+++ meu_codigo.py
@@ -1,2 +1,12 @@
 # macaco-bananas.
 Meu primeiro programa em Python que divide bananas entre amigos.
+
+bananas = int(input("Quantas bananas você tem? "))
+amigos = int(input("Quantos amigos vão dividir? "))
+
+sobras = bananas % amigos
+
+if sobras == 0:
+    print("Deu para todos certinho")
+else:
+    print("Sobrou", sobras, "banana(s)")
